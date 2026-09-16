a8766d463f6ebeb56c7d5fccde8830e54087ac4a
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-16 14:41:30 +0200

    Detect Windows and Linux desktop platform

--- shared/src/desktopMain/kotlin/com/freetime/lumastore/shared/Platform.desktop.kt
+++ shared/src/desktopMain/kotlin/com/freetime/lumastore/shared/Platform.desktop.kt
@@ -1,3 +1,9 @@
 package com.freetime.lumastore.shared
 
-actual val currentPlatform: LumaPlatform = LumaPlatform.DESKTOP
+private val osName: String = System.getProperty("os.name").lowercase()
+
+actual val currentPlatform: LumaPlatform = when {
+    osName.contains("win") -> LumaPlatform.WINDOWS
+    osName.contains("linux") -> LumaPlatform.LINUX
+    else -> error("Unsupported desktop platform: ${System.getProperty("os.name")}")
+}
