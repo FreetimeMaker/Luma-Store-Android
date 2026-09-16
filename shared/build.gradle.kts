4ff2e492f28b855e9e199681ba31424c46062a23
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-16 14:35:23 +0200

    Add shared Kotlin Multiplatform module

---
+++ shared/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.multiplatform.library)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.freetime.lumastore.shared"
+        compileSdk = 37
+        minSdk = 24
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+}
