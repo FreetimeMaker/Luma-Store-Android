7fc33fa904becb73a00fa724ecc2faa62121e654
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-16 14:36:03 +0200

    Add Compose Multiplatform desktop application

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.kotlin.compose)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.freetime.lumastore.desktop.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Exe, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Rpm)
+            packageName = "Luma Store"
+            packageVersion = "1.0.3"
+            description = "Luma Store desktop client"
+            vendor = "FreetimeMaker"
+        }
+    }
+}
