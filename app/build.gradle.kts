f873b0cbae938309d56e46c250c23c7ac85c1080
Jamie Achatz <jamieachatz@gmail.com>
2026-09-18 12:16:42 +0200

    Bump version to 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.freetime.lumastore"
         minSdk = 24
         targetSdk = 37
-        versionCode = 12
-        versionName = "3.0.0"
+        versionCode = 13
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
