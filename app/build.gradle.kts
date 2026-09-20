c2e648f3c6a0c5d377bf97aff0ae29148a3217b0
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-20 18:05:08 +0200

    Use only Freetime Design module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,10 +59,7 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.ktor:ktor-client-android:3.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
-    implementation(libs.freetime.core)
     implementation(libs.freetime.design)
-    implementation(libs.freetime.browser)
-    implementation(libs.freetime.donations)
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 
     implementation("io.coil-kt.coil3:coil-compose:3.6.2")
