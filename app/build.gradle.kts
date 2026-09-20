5fe9f26ef1fc4071afc2dfb798b80288996085b2
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-20 18:38:45 +0200

    Bump version to 4.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.freetime.lumastore"
         minSdk = 24
         targetSdk = 37
-        versionCode = 19
-        versionName = "4.2.0"
+        versionCode = 20
+        versionName = "4.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
