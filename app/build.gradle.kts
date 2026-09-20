89b946dd1e9d6fceed002bd7e2cd6382c7246ad2
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-20 21:26:55 +0200

    Prepare version 4.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.freetime.lumastore"
         minSdk = 24
         targetSdk = 37
-        versionCode = 21
-        versionName = "4.3.0"
+        versionCode = 22
+        versionName = "4.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
