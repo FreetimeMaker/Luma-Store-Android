da013b817febfa412a94681fa507023145121109
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-24 08:30:03 +0200

    Fix Freetime Design 1.9 imports

--- app/src/main/java/com/freetime/lumastore/AppDetailsScreen.kt
+++ app/src/main/java/com/freetime/lumastore/AppDetailsScreen.kt
@@ -1,6 +1,6 @@
 package com.freetime.lumastore
 
-import me.free_time.design.freetimeGlass
+import com.freetime.design.freetimeGlass
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
