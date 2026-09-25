ee20672ae732321958e4657d2b4f254fe1e1909b
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-25 13:40:40 +0200

    Migrate to Freetime Design 2.0 glass API

--- app/src/main/java/com/freetime/lumastore/FdroidStoreScreen.kt
+++ app/src/main/java/com/freetime/lumastore/FdroidStoreScreen.kt
@@ -2,7 +2,7 @@ package com.freetime.lumastore
 
 import android.content.Intent
 
-import com.freetime.design.freetimeGlass
+import com.freetime.design.liquidGlass
 
 import android.content.Context
 import androidx.compose.foundation.background
@@ -506,7 +506,7 @@ fun FdroidStoreScreen(
         val dialogShape = RoundedCornerShape(28.dp)
         AlertDialog(
             onDismissRequest = { selectedScreenshotUrl = null },
-            modifier = Modifier.freetimeGlass(dialogShape, interactive = false),
+            modifier = Modifier.liquidGlass(dialogShape, interactive = false),
             shape = dialogShape,
             containerColor = Color.Transparent,
             tonalElevation = 0.dp,
