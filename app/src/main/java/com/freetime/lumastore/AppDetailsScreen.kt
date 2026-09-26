fe3496320b9b61a8212b0e84976484107dd36156
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-26 12:15:50 +0200

    Fix app details progress indicator import

--- app/src/main/java/com/freetime/lumastore/AppDetailsScreen.kt
+++ app/src/main/java/com/freetime/lumastore/AppDetailsScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
