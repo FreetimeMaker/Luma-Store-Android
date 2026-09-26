a248c6bd1e3d28f4aaa15cc9cefec5c67dcb5a00
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-26 12:00:12 +0200

    Fix glass navigation Compose imports

--- app/src/main/java/com/freetime/lumastore/MainActivity.kt
+++ app/src/main/java/com/freetime/lumastore/MainActivity.kt
@@ -18,12 +18,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
